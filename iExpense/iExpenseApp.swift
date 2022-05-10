68370f647577db1ba478ac8f22c0ab312b716c4b
Shokri <shokre4@hotmail.com>
2022-05-10 17:20:55 +0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Shokri Alnajjar on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
